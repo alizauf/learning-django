7fd854e1ef6decf7a0df496ae4a45376e6d7e072
Aliza <aliza.aufrichtig@gmail.com>
2015-03-17 19:46:43 -0400

    added templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,20 @@
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>");
-    
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am the coolest bold person ever"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/index.html', context_dict)
+
 def about(request):
 	return HttpResponse("About us! <br /><a href='/rango/'>Let's go back to the index.</a>")
