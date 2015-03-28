1eba759a439ba878d54f8874b53fb1c03ba252bb
Aliza <aliza.aufrichtig@gmail.com>
2015-03-28 17:16:27 -0400

    Added a form

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,13 +8,40 @@ from rango.models import Category
 # Import the Page model
 from rango.models import Page
 
+from rango.forms import CategoryForm
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
 def index(request):
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
+    category_list = Category.objects.order_by('-likes')[:10]
+    page_list = Page.objects.order_by('-views')[:10]
     context_dict = {'categories': category_list, 'pages': page_list}
     
     
