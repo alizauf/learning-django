541d01f155151c083b9b6f8f173d31c3744ffe33
Aliza <aliza.aufrichtig@gmail.com>
2015-03-28 15:21:55 -0400

    Configured the admin

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+class CategoryAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['name']}),
+		('Interaction Information', {'fields': ['likes', 'views']}),
+		]
+	list_display = ('name', 'likes', 'views')
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+	fields = ['title', 'category', 'url', 'views']	
+	list_display = ('title', 'category', 'url', 'views')
+
+admin.site.register(Page, PageAdmin)
+	
+
+
+#simple version
+#admin.site.register(Category)
+#admin.site.register(Page)
